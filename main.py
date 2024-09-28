8fc3c14ebcb12a1a9f943e3e402ad4fa6f3fa8ff
Elnakib <kareemahmedelnakib04@gmail.com>
2024-09-28 17:59:24 +0300

    writing what we need in a to do list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+'''Python to do list'''
 
+'''What do we need in a To - do list????'''
+
+
+'''
+1 - inputting the tasks
+2 - deleting the tasks
+3 - checking the tasks 
+4 - database to track progress and analyze efficiency
+'''
