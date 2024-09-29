60e0ff2bc01f6632bba139dcd9d6dcc6ae434f52
Elnakib <kareemahmedelnakib04@gmail.com>
2024-09-29 04:54:53 +0300

    added the conditions in which the to do list will operate with four choices for the user and an array for the tasks

--- main.py
+++ main.py
@@ -9,6 +9,19 @@
 3 - checking the tasks 
 4 - database to track progress and analyze efficiency
 '''
+'''creating array for tasks'''
+todo = [] 
 
-task = input("Type the Task: ")
-print(task)
+if __name__ == "__main__" :
+  ### create a loop to run the app
+  print("Welcome to the to so list app :")
+
+  while True :
+    print("\n")
+    print("Please select one of the following options")
+    print("------------------------------------------")
+
+    print("1. Add a new task")
+    print("2. Delete a task")
+    print("3. List tasks")
+    print("4. Quit")
